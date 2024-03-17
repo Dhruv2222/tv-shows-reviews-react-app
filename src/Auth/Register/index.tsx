9e6bd53f7266b1ec79f858e50f684efcbe72954e
Dhruv Doshi <dhruv.doshi@somaiya.edu>
2024-03-17 12:09:06 -0400

    Added HTML for registration

--- src/Auth/Register/index.tsx
+++ src/Auth/Register/index.tsx
@@ -1,12 +1,44 @@
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Register() {
 
     return (
     <>
     
-    <h1>Register page</h1>
-    <Link to={"/Auth/Login"}>Login</Link>
+    
+    <div className="container">
+        <h1>Registration Form</h1>
+        <form action="#" method="post">
+            <div className="form-group">
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" name="username" required/>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" required/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="passwordConfirm">Confirm password:</label>
+                <input type="password" id="passwordConfirm" name="password" required/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="userType">Select role:</label>
+                <select id="userType" name="userType">
+                    <option value="user">User</option>
+                    <option value="moderator">Moderator</option>
+                    <option value="admin">Admin</option>
+                </select>
+            </div>
+            <div className="form-group">
+                <button className="" type="submit">Register</button>
+            </div>
+        </form>
+        <Link to={"/Auth/Login"}>Already have an account? Login</Link>
+    </div>
+    
     
     </>
     );
