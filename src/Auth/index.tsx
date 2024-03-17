c7554efa3846f581cdea1adb651f5188bd2ea8a7
aswath160598 <aswath.senthil@gmail.com>
2024-03-17 13:07:50 -0400

    Login Page

--- src/Auth/index.tsx
+++ src/Auth/index.tsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router";
 // import Assignment3 from "./a3";
 // import Assignment4 from "./a4";
 // import store from "./store";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 import Login from "./Login";
 import Register from "./Register";
 
@@ -13,7 +13,7 @@ function Auth() {
     <div>
       <Routes>
         <Route path="/"
-         element={<Navigate to="Login" />}/>
+         element={<Navigate to="Register" />}/>
          <Route path="Login"
          element={<Login />}/>
         <Route path="Register"
